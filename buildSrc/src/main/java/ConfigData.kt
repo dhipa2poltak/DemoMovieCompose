9eee8c12348f28c64b5fbe8f3ef726a4c1cb746c
Dhipa Poltak <dhipa2poltak@gmail.com>
2023-12-10 17:52:30 +0700

    increasing app version number

--- buildSrc/src/main/java/ConfigData.kt
+++ buildSrc/src/main/java/ConfigData.kt
@@ -2,8 +2,8 @@ object ConfigData {
   const val compileSdkVersion = 34
   const val minSdkVersion = 21
   const val targetSdkVersion = 34
-  const val versionCode = 1
-  const val versionName = "1.1"
-  const val versionCodeDev = 1
-  const val versionNameDev = "1.1"
+  const val versionCode = 2
+  const val versionName = "1.2"
+  const val versionCodeDev = 2
+  const val versionNameDev = "1.2"
 }
